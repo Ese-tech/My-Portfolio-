8f50e07fe0faaa013c9e67c6f3ea019abce1de4e
Ese-Tech <eseosagie22@gmail.com>
2025-03-18 13:37:05 +0100

    Add AboutMe, Contact, Home, and MyProjects components with basic structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
-import
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import MyProjects from './components/MyProjects';
+import Contact from './components/Contact';
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
