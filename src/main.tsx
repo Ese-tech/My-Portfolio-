38604f6925fbb6b85a5eb1b8239ad31c0856314e
Ese-Tech <eseosagie22@gmail.com>
2025-03-18 13:25:36 +0100

    Add routing and create initial components for Home, AboutMe, MyProjects, and Contact

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/my-projcts" element={<MyProjects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
