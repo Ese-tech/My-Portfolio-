a3bcb01b773f83a402829fa6cfe9a0dff15629a4
Ese-Tech <eseosagie22@gmail.com>
2025-03-21 21:49:43 +0100

    Refactor NavBar component for improved layout and styling, enhancing responsiveness and visual appeal

--- src/features/navigation/NavBar.tsx
+++ src/features/navigation/NavBar.tsx
@@ -11,57 +11,78 @@ export function NavBar() {
 	};
 
 	return (
-		<div className='bg-gradient-to-r from-amber-600 to-yellow-500 border-2'>
-			
-			<nav
-				className={`${
-					isSidebarOpen ? "flex" : "hidden"
-				} flex-col md:flex md:flex-row md:space-x-15 p-5`}
-			>
-				<NavLink
-					to='/'
-					end
-					className={({ isActive }) =>
-						isActive
-							? "text-gold-500 font-bold"
-							: "text-zinc-300 hover:text-gold-500"
-					}
-				>
-					Home
-				</NavLink>
-				<NavLink
-					to='/about-me'
-					end
-					className='text-zinc-300 hover:text-gold-500'
-				>
-					About Me
-				</NavLink>
-				<NavLink
-					to='/my-projects'
-					end
-					className='text-zinc-300 hover:text-gold-500'
-				>
-					My Projects
-				</NavLink>
-				<NavLink
-					to='/contact'
-					end
-					className='text-zinc-300 hover:text-gold-500'
-				>
-					Contact
-				</NavLink>
-				<NavLink to='/skills' end className='text-zinc-300 hover:text-gold-500'>
-					Skills
-				</NavLink>
-      </nav>
-      <div className='flex justify-between items-center p-5'>
-				<h1 className='text-white font-bold text-xl'>My Portfolio</h1>
+		<div className='bg-gradient-to-r from-amber-600 to-yellow-500 border-2 '>
+			<div className='flex justify-between items-center p-5'>
 				<button
 					className='text-white text-2xl md:hidden'
 					onClick={toggleSidebar}
 				>
 					<FontAwesomeIcon icon={isSidebarOpen ? faTimes : faBars} />
 				</button>
+			</div>
+			<div className='flex justify-center'>
+				<nav
+					className={`${
+						isSidebarOpen ? "flex" : "hidden"
+					} flex-col md:flex md:flex-row md:space-x-15 p-5 bg-white rounded-lg shadow-lg `}
+				>
+					<NavLink
+						to='/'
+						end
+						className={({ isActive }) =>
+							isActive ? "text-gold-500 font-bold" : "hover:text-gold-500"
+						}
+					>
+						Home
+					</NavLink>
+					<NavLink
+						to='/about-me'
+						end
+						className={({ isActive }) =>
+							isActive
+								? "text-amber-700 font-bold"
+								: "text-zinc-500 hover:text-gold-500"
+						}
+					>
+						About Me
+					</NavLink>
+					<NavLink
+						to='/my-projects'
+						end
+						className={({ isActive }) =>
+							isActive
+								? "text-amber-700 font-bold"
+								: "text-zinc-500 hover:text-gold-500"
+						}
+					>
+						My Projects
+					</NavLink>
+					<NavLink
+						to='/contact'
+						end
+						className={({ isActive }) =>
+							isActive
+								? "text-amber-700 font-bold"
+								: "text-zinc-500 hover:text-gold-500"
+						}
+					>
+						Contact
+					</NavLink>
+					<NavLink
+						to='/skills'
+						end
+						className={({ isActive }) =>
+							isActive
+								? "text-amber-700 font-bold"
+								: "text-zinc-500 hover:text-gold-500"
+						}
+					>
+						Skills
+					</NavLink>
+				</nav>
+			</div>
+			<div className='flex justify-center mt-5'>
+				<h1 className='text-white font-bold text-xl'>My Portfolio</h1>
 			</div>
 		</div>
 	);
