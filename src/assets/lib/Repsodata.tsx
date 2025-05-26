1f9572c81316c52d29ecf576ceb19dff5de436a7
Ese-tech <ese.osagie19@gmail.com>
2025-05-26 18:27:36 +0200

    Add skills and repository data for web development projects

---
+++ src/assets/lib/Repsodata.tsx
@@ -0,0 +1,97 @@
+
+export const skillsRow1 = [
+  { name: "HTML", icon: "/assets/icons/htmlicon.svg", color: "#F1662A" },
+  { name: "CSS", icon: "/assets/icons/cssicon.svg", color: "#1572B6" },
+  { name: "JavaScript", icon: "/assets/icons/javascripticon.svg", color: "#F7DF1E" },
+  { name: "React", icon: "/assets/icons/reacticon.svg", color: "#61DAFB" },
+  { name: "Node.js", icon: "/assets/icons/nodejsicon.svg", color: "#339933" },
+  { name: "Git", icon: "/assets/icons/Git-Icon-1788C.svg", color: "#F05032" },
+  { name: "GitHub", icon: "/assets/icons/github-color.svg", color: "#181717" },
+  { name: "SASS", icon: "/assets/icons/sassicon.svg", color: "#CC6699" },
+];
+
+export const skillsRow2 = [
+  { name: "Responsive", icon: "/assets/icons/responsive-design.png", color: "#38B2AC" },
+  { name: "Terminal", icon: "/assets/icons/terminal-svgrepo-com.svg", color: "#000000" },
+  { name: "Tailwind", icon: "/assets/icons/tailwindicon.svg", color: "#38B2AC" },
+];
+
+export const skillsDataWeb = [
+  {
+    skillsTitle: "WebDevelopment",
+    skills: [
+      {
+        title: "HTML",
+        hash: "#html",
+        icon: "public/assets/icons/htmlicon.svg",
+        color: "#F1662A",
+      },
+      {
+        title: "CSS",
+        hash: "#CSS",
+        icon: "public/assets/icons/cssicon.svg",
+        color: "#1572B6",
+      },
+      {
+        title: "JavaScript",
+        hash: "#JavaScript",
+        icon: "public/assets/icons/javascripticon.svg",
+        color: "#F7DF1E",
+      },
+      {
+        title: "TypeScript",
+        hash: "#TypeScript",
+        icon: "public/assets/icons/typescripticon.svg",
+        color: "#007ACC",
+      },
+      {
+        title: "React",
+        hash: "#React",
+        icon: "public/assets/icons/reacticon.svg",
+        color: "#61DAFB",
+      },
+      {
+        title: "Node.js",
+        hash: "#Node.js",
+        icon: "public/assets/icons/nodejsicon.svg",
+        color: "#339933",
+      },
+      {
+        title: "Tailwind",
+        hash: "#Tailwind",
+        icon: "/assets/icons/tailwindicon.svg",
+        color: "#38B2AC",
+      },
+      {
+        title: "SASS/SCSS",
+        hash: "#SASS/SCSS",
+        icon: "public/assets/icons/sassicon.svg",
+        color: "#CC6699",
+      },
+    ],
+  },
+] as const;
+
+export const reposdata =[
+ {
+    name: "Color-Changer",
+    description: "A simple color changer app",
+    image: "",
+    technologiesTags: ["Typescript","Tailwind CSS","Bun"],
+    links: {
+        githubUrl: "https://github.com/Ese-tech/Color-Changer",
+        vercelUrl: "https://color-changer-three-blue.vercel.app/"
+    }
+ },
+ {
+    name: "Todo-App",
+    description: "A simple todo app",
+    image: "",
+    technologiesTags: ["Typescript","Tailwind CSS","Bun"],
+    links: {
+        githubUrl: "https://github.com/Ese-tech/Todo-app",
+        vercelUrl: "https://todo-app-sable-tau.vercel.app/"
+    }
+ }
+
+]
--- src/features/pages/Skills.tsx
+++ src/features/pages/Skills.tsx
@@ -1,5 +1,55 @@
-function Skills() {
-	return <>Skills</>;
-}
+
+import { skillsRow1, skillsRow2 } from "../../assets/lib/Repsodata";
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <div className="skills-header">
+        <h1 className="skills-title">
+          <span className="skills-title-my">My</span> <span className="skills-title-skills">Skills</span>
+        </h1>
+        <p className="projects-subtitle">
+          Discover the skills that shape my journey as a web developer. <br /> Each technology and tool I’ve mastered reflects
+          my commitment to creating high-quality, innovative solutions.
+        </p>
+      </div>
+      <div className="skills-row">
+        {skillsRow1.map((skill, index) => (
+          <div
+            key={index}
+            className="skill-icon-container"
+            style={{ animationDelay: `${index * 2}s` }}
+          >
+            <img
+              src={skill.icon}
+              alt={skill.name}
+              className="skill-icon"
+              style={{ color: skill.color }}
+            />
+            <p className="skill-name">{skill.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="skills-row">
+        {skillsRow2.map((skill, index) => (
+          <div
+            key={index}
+            className="skill-icon-container"
+            style={{ animationDelay: `${index * 2}s` }}
+          >
+            <img
+              src={skill.icon}
+              alt={skill.name}
+              className="skill-icon"
+              style={{ color: skill.color }}
+            />
+            <p className="skill-name">{skill.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="line2-skills">Skills that shape my work</div>
+    </div>
+  );
+};
 
 export default Skills;
