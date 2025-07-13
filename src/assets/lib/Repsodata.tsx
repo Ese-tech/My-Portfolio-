aee040902556a1e0f1594c24d80440050ed49bb5
Ese-tech <ese.osagie19@gmail.com>
2025-07-13 14:44:59 +0200

    Add Repsodata.tsx for home page implementation

---
+++ src/assets/lib/Repsodata.tsx
@@ -0,0 +1,215 @@
+export const skillsRow1 = [
+  {
+    name: "HTML",
+    icon: "/assets/icons/htmlicon.svg",
+    color: "#F1662A",
+    category: "Frontend",
+    level: "Advanced",
+  },
+  {
+    name: "CSS",
+    icon: "/assets/icons/cssicon.svg",
+    color: "#1572B6",
+    category: "Frontend",
+    level: "Advanced",
+  },
+  {
+    name: "JavaScript",
+    icon: "/assets/icons/javascripticon.svg",
+    color: "#F7DF1E",
+    category: "Frontend",
+    level: "Advanced",
+  },
+  {
+    name: "React",
+    icon: "/assets/icons/reacticon.svg",
+    color: "#61DAFB",
+    category: "Frontend",
+    level: "Intermediate",
+  },
+
+  {
+    name: "TypeScript",
+    icon: "/assets/icons/typescripticon.svg",
+    color: "#007ACC",
+    category: "Frontend",
+    level: "Intermediate",
+  },
+  {
+    name: "Node.js",
+    icon: "/assets/icons/nodejsicon.svg",
+    color: "#339933",
+    category: "Backend",
+    level: "Intermediate",
+  },
+  {
+    name: "Next.js",
+    icon: "/assets/icons/nextjsicon.svg",
+    color: "#000000",
+    category: "Backend",
+    level: "Intermediate",
+  },
+  {
+    name: "Express.js",
+    icon: "/assets/icons/expressicon.svg",
+    color: "#000000",
+    category: "Backend",
+    level: "Intermediate",
+  },
+  {
+    name: "MongoDB",
+    icon: "/assets/icons/mongoicon.svg",
+    color: "#47A248",
+    category: "Backend",
+    level: "Intermediate",
+  },
+    {
+    name: "Tailwind",
+    icon: "/assets/icons/tailwindicon.svg",
+    color: "#38B2AC",
+    category: "Frontend",
+    level: "Intermediate",
+  },
+  {
+    name: "SASS/SCSS",
+    icon: "/assets/icons/sassicon.svg",
+    color: "#CC6699",
+    category: "Frontend",
+    level: "Beginner",
+  },
+];
+
+export const skillsRow2 = [
+  {
+    name: "Responsive",
+    icon: "/assets/image/responsive-design.png",
+    color: "#38B2AC",
+    category: "Frontend",
+    level: "Advanced",
+  },
+    {
+    name: "GitHub",
+    icon: "/assets/icons/github-color.svg",
+    color: "#181717",
+    category: "Tools",
+    level: "Intermediate",
+  },
+  {
+    name: "Terminal",
+    icon: "/assets/icons/terminal-svgrepo-com.svg",
+    color: "#000000",
+    category: "Tools",
+    level: "Intermediate",
+  },
+    {
+    name: "Git",
+    icon: "/assets/icons/Git-Icon-1788C.svg",
+    color: "#F05032",
+    category: "Tools",
+    level: "Intermediate",
+  },
+
+];
+
+export const softSkills = [
+  { name: "Communication", icon: "/assets/image/networking.png" },
+  { name: "Teamwork", icon: "/assets/image/teamwork.png" },
+  { name: "Problem Solving", icon: "/assets/image/problem-solving.png" },
+  { name: "Adaptability", icon: "/assets/image/adaptability.png" },
+  { name: "Time Management", icon: "/assets/image/time-management.png" },
+  { name: "Creativity", icon: "/assets/image/creative.png" },
+  { name: "Critical Thinking", icon: "/assets/image/critical-thinking.png" },
+];
+
+export const skillsDataWeb = [
+  {
+    skillsTitle: "WebDevelopment",
+    skills: [
+      {
+        title: "HTML",
+        hash: "#html",
+        icon: "public/assets/icons/htmlicon.svg",
+        color: "#F1662A",
+      },
+      {
+        title: "CSS",
+        hash: "#CSS",
+        icon: "public/assets/icons/cssicon.svg",
+        color: "#1572B6",
+      },
+      {
+        title: "JavaScript",
+        hash: "#JavaScript",
+        icon: "public/assets/icons/javascripticon.svg",
+        color: "#F7DF1E",
+      },
+      {
+        title: "TypeScript",
+        hash: "#TypeScript",
+        icon: "public/assets/icons/typescripticon.svg",
+        color: "#007ACC",
+      },
+      {
+        title: "React",
+        hash: "#React",
+        icon: "public/assets/icons/reacticon.svg",
+        color: "#61DAFB",
+      },
+      {
+        title: "Node.js",
+        hash: "#Node.js",
+        icon: "public/assets/icons/nodejsicon.svg",
+        color: "#339933",
+      },
+      {
+        title: "Tailwind",
+        hash: "#Tailwind",
+        icon: "/assets/icons/tailwindicon.svg",
+        color: "#38B2AC",
+      },
+      {
+        title: "SASS/SCSS",
+        hash: "#SASS/SCSS",
+        icon: "public/assets/icons/sassicon.svg",
+        color: "#CC6699",
+      },
+    ],
+  },
+] as const;
+
+export const reposdata = [
+  {
+    id: 1,
+    name: "Color-Changer",
+    description: "A simple color changer app",
+    image: "",
+    technologiesTags: ["Typescript", "Tailwind CSS", "Bun"],
+    links: {
+      githubUrl: "https://github.com/Ese-tech/Color-Changer",
+      vercelUrl: "https://ese-tech.github.io/Color-Changer/",
+    },
+    html_url: "https://github.com/Ese-tech/Color-Changer",
+    stargazers_count: 0,
+    topics: ["color", "app"],
+    created_at: "2024-01-01T00:00:00Z",
+    updated_at: "2024-01-02T00:00:00Z",
+    language: "TypeScript",
+  },
+  {
+    id: 2,
+    name: "Todo-App",
+    description: "A simple todo app",
+    image: "",
+    technologiesTags: ["Typescript", "Tailwind CSS", "Bun"],
+    links: {
+      githubUrl: "https://github.com/Ese-tech/Todo-app",
+      vercelUrl: "https://todo-app-sable-tau.vercel.app/",
+    },
+    html_url: "https://github.com/Ese-tech/Todo-app",
+    stargazers_count: 0,
+    topics: ["todo", "app"],
+    created_at: "2024-01-01T00:00:00Z",
+    updated_at: "2024-01-02T00:00:00Z",
+    language: "TypeScript",
+  },
+];
